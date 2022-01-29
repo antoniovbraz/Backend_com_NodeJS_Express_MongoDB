81d285ab4f90a24ba56490999d1e1ea23d3cca08
Antonio Braz <antoniovbraz@gmail.com>
2022-01-29 18:11:46 -0300

    arquivo index,js criado, express e nodemon adicionados como dependências e scripts start/dev criados

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+const port = 3000;
+app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.info(`App rodando em http://localhost:${port}`)
+})
